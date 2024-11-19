d5edaff0eab4b41fd3f0338c952c5f59eaba80e0
iiooana <ioana2502@yahoo.it>
2024-11-19 10:30:29 +0100

    init

---
+++ code/tailwind.config.js
@@ -0,0 +1,35 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+    ],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+        colors: {
+            'blue-whale': {
+                '50': '#f0f9ff',
+                '100': '#e0f2fe',
+                '200': '#bae2fd',
+                '300': '#7dc9fc',
+                '400': '#38abf8',
+                '500': '#0e91e9',
+                '600': '#0278c7',
+                '700': '#0362a1',
+                '800': '#075385',
+                '900': '#0c476e',
+                '950': '#082f49',
+            },
+        }
+    },
+    plugins: [],
+};
