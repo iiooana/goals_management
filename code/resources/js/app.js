520a07debc4d50cd42d0fcf0e8ee358d0493ef29
iiooana <ioana2502@yahoo.it>
2024-11-20 14:15:49 +0100

    add dark and light mode

--- code/resources/js/app.js
+++ code/resources/js/app.js
@@ -1,2 +1,40 @@
 import './bootstrap';
 import 'font-awesome/css/font-awesome.min.css';
+
+const html = document.getElementsByTagName("html")[0];
+const icon =  document.getElementById("icon-theme");
+if(localStorage.getItem('theme-color') === null){
+    localStorage.setItem("theme-color","dark");
+}
+updateTheme();
+
+
+/**
+ *  function region
+ */
+function updateTheme(){
+    switch(localStorage.getItem('theme-color'))  {
+        case "light":
+            html.classList.remove("dark");
+            icon.classList.remove("fa-sun-o");
+            icon.classList.add("fa-moon-o");    
+            break;
+        default:
+            html.classList.add("dark");
+            icon.classList.add("fa-sun-o");
+            icon.classList.remove("fa-moon-o");    
+            break;    
+    }
+}
+function toogleTheme(){
+    if(html.classList.contains("dark")){
+        localStorage.setItem("theme-color","light");
+    }else{
+        localStorage.setItem("theme-color","dark");
+    }
+    updateTheme();
+}
+/**
+ *  end function region
+ */
+window.toogleTheme = toogleTheme;
--- code/resources/views/welcome.blade.php
+++ code/resources/views/welcome.blade.php
@@ -10,32 +10,34 @@
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-
     <!-- Styles / Scripts -->
     @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @endif
+
 </head>
 
-<body class="dark first-letter:dark:bg-bluew-950">
-    <nav class="bg-bluew-900/40 text-slate-300 min-h-16 text-lg font-medium tracking-wider shadow-sm">
-        <div class="columns-4">
-            <div>
-                <a href="/" class="">
-                    <img class="max-h-14" src="/img/icons8-goals-lineal-color-96.png">
-                </a>
-            </div>
-            <div>
-                <a href="/" class="btn-navbar">Home</a>
-                <a href="/" class="btn-navbar">Goals list</a>
-                <button id="btn-theme" class="btn-navbar">asa
-                <i class="fa fa-sun-o text-xl"></i> 
-                </button>
-            </div>
+<body class="bg-bluew-100 dark:bg-slate-950">
+
+    <nav class="bg-bluew-300/30 dark:bg-bluew-900/10 text-gray-800 dark:text-slate-100 min-h-16 text-xl font-bold shadow-md flex flex-row items-center uppercase tracking-tight">
+        <div class="basis-1/4">
+            <a href="/" class="basic-6">
+                <img class="max-h-14" src="/img/icons8-goals-lineal-color-96.png">
+            </a>
         </div>
-    </nav> 
+        <div class="basis-3/4">
+            <a href="/" class="btn-navbar basic-2/4">Home</a>
+            <a href="/" class="btn-navbar basic-2/4">List</a>
+        </div>
+        <div class="basis-1/2 text-right">
+        <button class="py-2 px-4 basic-2/4 " onclick="toogleTheme()">
+                <i id="icon-theme" class="fa fa-sun-o text-bluew-950 dark:text-amber-100 text-2xl"></i>
+            </button>
+        </div>
+
+    </nav>
 
 
 </body>
+
 </html>
