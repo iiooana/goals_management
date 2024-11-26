86b9701b8eb113519b9362d955536c8b5f8221a9
iiooana <ioana2502@yahoo.it>
2024-11-26 13:56:41 +0100

    fix errore message

--- code/resources/views/livewire/goal.blade.php
+++ code/resources/views/livewire/goal.blade.php
@@ -1,9 +1,9 @@
 <div class="div-template rounded-lg border border-slate-400 dark:border-slate-200">
     <form wire:submit="save">
         <div class="grid grid-cols-2 gap-3">
-            <x-input-text name="title" label="title" required="true" wire:model="form.title" />
-            <x-input-datetime name="deadline" label="deadline" min="{{ $is_new === true? date('Y-m-d').'T'.date('H:i') : ''}}" wire:model="form.deadline" />        
-            <x-input-textarea name="description" label="description" wire:model="form.description" />
+            <x-input-text name="form.title" label="title" required="true" wire:model="form.title" />
+            <x-input-datetime name="form.deadline" label="deadline" min="{{ $is_new === true? date('Y-m-d').'T'.date('H:i') : ''}}" wire:model="form.deadline" />        
+            <x-input-textarea name="form.description" label="description" wire:model="form.description" />
         </div>
         <div wire:loading.delay>
             <div class="flex justify-center bg-amber-500 my-3 py-2 px-1 text-center font-semibold leading-6 text-sm shadow-inner rounded-lg dark:text-slate-950 transition ease-in-out duration-150">
