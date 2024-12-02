27dfa36f4980524c4ee5bf7be7f497a2680c1af4
iiooana <ioana2502@yahoo.it>
2024-12-02 21:18:31 +0100

    Quiz module

---
+++ code/database/migrations/2024_12_02_210322_create_quizzes_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('quizzes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('subject_id');
+            $table->integer('n_questions');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('subject_id')->on('subjects')->references('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('quizzes');
+    }
+};
