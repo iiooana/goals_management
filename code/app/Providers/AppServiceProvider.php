54c24970b0d4d64733a886c2d54e5f9b2f4e05d7
iiooana <ioana2502@yahoo.it>
2024-12-30 15:07:45 +0100

    Add prefetch

--- code/app/Providers/AppServiceProvider.php
+++ code/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        //prefetch 3 assets before each page load
+        Vite::prefetch(concurrency:3);
     }
 }
